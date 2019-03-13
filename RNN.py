470e143b1069dc4cde67d911351c01adb48ab597
tongx11 <46943084+tongx11@users.noreply.github.com>
2019-03-13 01:17:02 -0700

    Add files via upload

---
+++ RNN.py
@@ -0,0 +1,76 @@
+import numpy as np
+import random
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Embedding, LSTM, Lambda, Activation
+from RNN_utility import poem_to_id, sentence_to_id, RNN_data_generator, poem_composer
+
+
+corpus, dict = poem_to_id('data/shakespeare4.txt')
+dict_len = len(dict)
+
+
+# fix sequence length to 40 characters as required
+seq_size = 40
+# take sequence samples every 3 characters
+skip_step = 3
+batch_size = int(round((len(corpus)-seq_size)*1.0/skip_step))
+print('batch_size = ', batch_size)
+# generate sample x and y from shakespeare's poems
+x, y = RNN_data_generator(corpus, seq_size, batch_size, dict_len, skip_step)
+
+# hidden_size within range of 100-200
+hidden_size = 150
+
+# modify the temperature to obtain different probabiliy distribution
+temp_list = [0.75]#[0.25, 0.75, 1.5]
+for temp in temp_list:
+    # Model created with constraints in the problem
+    model = Sequential()
+    # a single layer of 150 LSTM units are applied as instructed
+    model.add(LSTM(hidden_size,input_shape=(seq_size, dict_len)))
+
+    model.add(Lambda(lambda x: x / temp))
+    # a fully connected output layer with softmax nonlinearity
+    model.add(Dense(dict_len, activation = 'softmax'))
+
+    # Print a summary of the layers and weights in model
+    model.summary()
+
+    # with one-hot encoding the labels, use 'categorical_crossentropy' as the loss
+    model.compile(loss='categorical_crossentropy',optimizer='adam', metrics=['categorical_accuracy'])
+
+    fit = model.fit(x, y, batch_size=30, epochs=40, verbose=1)
+
+    model_file = 'model_t'+str(temp)+'.h5'
+    model.save_weights(model_file)
+
+    # seed sentence
+    seed = "shall i compare thee to a summer's day?\n"
+    seed_id = sentence_to_id(seed, dict)
+    seed_sen = np.zeros((1, seq_size, dict_len))
+    seed_sen[0,:,:] = seed_id
+    poem_len = 800
+    pseudo_poem = poem_composer(model, seed_sen, poem_len, seq_size, dict, temp)
+    print(pseudo_poem)
+
+
+#for temp in temp_list:
+#    model = Sequential()
+    # a single layer of 150 LSTM units are applied as instructed
+#    model.add(LSTM(hidden_size,input_shape=(seq_size, dict_len)))
+
+#    model.add(Lambda(lambda x: x / temp))
+    # a fully connected output layer with softmax nonlinearity
+#    model.add(Dense(dict_len, activation = 'softmax'))
+
+    # Print a summary of the layers and weights in model
+#    model.summary()
+
+    # with one-hot encoding the labels, use 'categorical_crossentropy' as the loss
+#    model.compile(loss='categorical_crossentropy',optimizer='adam', metrics=['categorical_accuracy'])
+
+#    model_file = 'model_t'+str(temp)+'.h5'
+#    model.load_weights(model_file)
+#    pseudo_poem = poem_composer(model, seed_sen, poem_len, seq_size, dict, temp)
+#    print(pseudo_poem)
---
+++ RNN_utility.py
@@ -0,0 +1,129 @@
+import os
+import re
+import numpy as np
+import tensorflow as tf
+from keras.utils import to_categorical
+
+def poem_to_id(filename):
+    '''
+    output: an array of sequence, each sequence is an array of encoded word for a poem, a stanza, or a line
+    '''
+
+    # Convert text to dataset.
+    text = open(os.path.join(os.getcwd(), filename)).read()
+    lines = [line.split() for line in text.split('\n') if line.split()]
+
+    char_counter = 0
+    char_idx = []
+    char_map = {}
+    raw_corpus = ''
+
+    for idx in range(len(lines)):
+        line = lines[idx]
+        for word_idx in range(len(line)):
+            word = line[word_idx]
+            for char in word:
+                if (char == ',') or (char == '.') or (char == '?') or (char == '!') or (char == ';') or (char == ':') or (char == '-') or ( char == "'" ):
+                    raw_corpus+= char
+                else:
+                    char = re.sub(r'[^\w]', '', char).lower()
+                    if char == '':
+                        continue
+                    raw_corpus+= char
+            if word_idx == len(line) - 1:
+                raw_corpus += '\n'
+            else:
+                raw_corpus += ' '
+
+    for char in raw_corpus:
+        if char not in char_map:
+            # Add unique words to the observations map.
+            char_map[char] = char_counter
+            char_counter += 1
+            # Add the encoded word.
+        char_idx.append(char_map[char])
+
+    return char_idx, char_map
+
+def sentence_to_id(seed, dict):
+    seed_id = []
+    dict_len = len(dict)
+    for char in seed:
+        id = dict[char]
+        tmp = to_categorical(id, num_classes = dict_len)
+        seed_id.append(tmp)
+    return seed_id
+
+def RNN_data_generator(corpus, seq_size, batch_size, dict_len, skip_step):
+    '''
+    generate input x and y from the whole corpus
+    poem = whole corpus mapped into integer index in order
+    seq_size = number of consecutive characters from corpus that forms a sequence
+    batch_size = number of sequences to train over
+    dict_len = length of dictionary built from the corups
+    skip_step = gap between consecutive sequences
+    '''
+
+    x = np.zeros((batch_size, seq_size, dict_len))
+    y = np.zeros((batch_size, dict_len))
+    corpus_len = len(corpus)
+    idx_start = 0
+    idx_end = 0
+    for idx in range(batch_size):
+        idx_start = idx_start % corpus_len
+        idx_end = idx_start + seq_size
+        if idx_end+1 >= corpus_len:
+            print('redundant sequences, choose smaller batch_size')
+            idx_start = int(round(skip_step/2.0))
+            idx_end = idx_start + seq_size
+        tmp_x = corpus[idx_start:idx_end]
+        x[idx, :, :] = to_categorical(tmp_x, num_classes=dict_len)
+        tmp_y = corpus[idx_end+1]
+        y[idx, :] = to_categorical(tmp_y, num_classes=dict_len)
+        idx_start += skip_step
+
+    return x, y
+
+def sortSecond(val):
+    return val[1]
+
+def id_to_char(id, dict):
+    '''
+    draw word from dictionary according to word idx
+    id_list = a list of word id in dictionary
+    dict = dictionary built from corpus
+    '''
+    dict_list = list(dict.items())
+    dict_list.sort(key = sortSecond)
+    char = dict_list[id][0]
+    return char
+
+
+def sample(preds, temperature=1.0):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+def poem_composer(model, seed_sen, poem_len, seq_size, dict, temperature):
+    '''
+    generate poems from RNN model
+    seed_sen = seed sentence fed into the model
+    poem_len = length of poem to write
+    '''
+    pseudo_poem = ''
+    dict_len = len(dict)
+    for idx in range(poem_len):
+        test = model.predict(seed_sen)
+        prediction = model.predict(seed_sen)[0]
+        id = sample(prediction, temperature)
+        #id = id_list[:,seq_size-1]
+        #id_list = np.argmax(prediction[:, seq_size-1, :])
+        pseudo_poem += id_to_char(id, dict)
+        next_sen = np.zeros((1, seq_size, dict_len))
+        next_sen[0,:seq_size-1,:] = seed_sen[0, 1:, :]
+        next_sen[0,seq_size-1,:] = to_categorical(id, num_classes=dict_len)
+        seed_sen = next_sen
+    return pseudo_poem
